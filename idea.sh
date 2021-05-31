5ffe69daae46691e8903599b4f7e81f8849cfb43
Trash-buket <85055259+Trash-buket@users.noreply.github.com>
2021-05-31 19:58:55 -0400

    Add files via upload

--- idea.sh
+++ idea.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-ls -al --color=auto ~/ideas || exit
+clear && ls -l --color=auto ~/ideas || exit
 
-echo "What would you like to do? (rmidea to remove a idea and mkidea to make one)"
+echo "What would you like to do? (type help to show working commands)"
 
 read response
 
-if [[ $response = rmidea ]]
+if [[ $response = rm ]]
 
 then
 	echo "What idea would you like to remove"
@@ -14,7 +14,7 @@ then
 	rm -r ~/ideas/$rmresponse && echo "The idea is removed" || echo "There isn't a idea with that name" && exit
 
 
-elif [[ $response = mkidea ]]
+elif [[ $response = mk ]]
 then
 	echo "What is the name of this idea?"
 	read ideaname
@@ -22,6 +22,32 @@ then
 	read ideadesc
 	echo $ideadesc > ~/ideas/$ideaname && echo "Nice, your idea is saved in your idea directory"
 
-else exit
+
+elif [[ $response = show ]]
+then
+	echo "what idea would you like to show?"
+	read showidea
+	cat ~/ideas/$showidea && sleep 5 && exec ~/projects/idea.sh
+
+
+elif [[ $response = exit ]]
+then
+	echo "you could also use ctrl+c" && exit
+
+
+elif [[ $response = edit ]]
+then
+	echo "what idea would you like to edit?"
+	read editresponse
+	vim ~/ideas/$editresponse
+
+
+elif [[ $response = help ]]
+then
+	echo "mk to make a idea, rm to remove a idea, edit to edit a idea (by default uses vim), show to read a idea and exit to exit" && sleep 5 && exec ~/projects/idea.sh
+
+
+
+else echo "please use a working command" && exit
 
 fi
